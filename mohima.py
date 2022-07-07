042dcc0950d240f4b273699c2eb7361387214227
aasma choudhary <94236134+aasma12345@users.noreply.github.com>
2022-07-07 15:15:50 +0530

    Add files via upload

---
+++ mohima.py
@@ -0,0 +1,58 @@
+# print("1 addition")
+# print("2 subtraction")
+# print("3 multiplication")
+# print("4 division")
+
+# choise=input("enter your choise: ")
+# num1=int(input("enter num1: "))
+# num2=int(input("enter num2: "))
+
+# if choise=="1":
+#     print(num1,"+",num2,"=",(num1+num2))
+# elif choise=="2":
+#     print(num1,"-",num2,"=",(num1-num2))
+# elif choise=="3":
+#     print(num1,"*",num2,"=",(num1*num2))
+# elif choise=="4":
+#     if num2==0.0:
+#         print("divide by 0 error")
+#     else:
+#         print(num1,"/",num2,"=",(num1/num2))
+# else:
+#     print("invalid choise")
+
+
+# i=7
+# j=1
+# while i>=1:
+#     print(" "*j,i*"*",j*" ")
+#     j=j+1
+#     i=i-2
+
+
+
+# i=1
+# while i<=7:
+#     print(" "*(7-i),end=" ")
+#     j=1
+#     while j<=i:
+#         print("*",end=" ")
+#         j=j+1
+#     print()
+#     i=i+i
+
+
+
+# i=7
+# j=1
+# while i>=1:
+#     print(" "*j,i*"*",j*" ")
+#     j=j-1
+#     i=i+2
+
+
+student_list=['aasma','manpreet','shavangi','anshika','fana']
+length_list=len(student_list)
+print(length_list)
+
+
